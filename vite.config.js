34bce3f4258aaa2733017c1f9be3d0a27aa97c85
Pritam Das <pritamdas.pd2005@gmail.com>
2025-11-16 00:15:50 +0530

    Add image optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteImageOptimizer({
+      // Optimize and convert images to WebP
+      webp: {
+        quality: 80,
+      },
+      // Also optimize PNG
+      png: {
+        quality: 80,
+      },
+      // Also optimize JPEG
+      jpeg: {
+        quality: 80,
+      },
+      // Also optimize JPG
+      jpg: {
+        quality: 80,
+      },
+    }),
+  ],
 });
